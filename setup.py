d8bf2bd110f710eb1c966f293f300f7e418c1eda
Victor Kotseruba <barbuzaster@gmail.com>
2016-06-23 14:28:30 -0700

    0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-robust',
-    version='0.1.7',
+    version='0.1.8',
     description='robust background queue for django',
     author='Victor Kotseruba',
     author_email='barbuzaster@gmail.com',
